cf73e506f1a0a520dac8f478b9a0cf86f16a0917
k0ntinuum <k0ntinuum@proton.me>
2023-11-08 05:28:20 -0500

    a

--- cyclone-jl.jl
+++ cyclone-jl.jl
@@ -1,29 +1,52 @@
 using Random
 using Printf
-using LinearAlgebra
 
-
-key(n) = copy(transpose(reduce(hcat, [Random.randperm(n) for i in 1:n])))
-
-str_from_vec(v)  = join(map(i -> alph[i:i], v))
+function str_from_vec(v,c)
+    #alph = "O|"
+    #alph = "abcdefghijklmnopqrstuvwxyz_"
+    alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+    join(map(i -> alph[i:i]*c, v))
+end
 
 function printkey(k)
-	for i in 1:n print(join(map(i -> alph[i:i]*" ", k[i,:])),"\n") end
+    n = size(k)[begin]
+	for i in 1:n print(str_from_vec( k[i,:]," " ),"\n") end
 	print("\n")
 end
 
-str_from_vec(v,c)  = join(map(i -> alph[i+1:i+1]*c, v))
+function vec_from_str(s)
+    map(i -> findfirst(isequal(i),alph),collect(s))
+end
 
-vec_from_str(s) = map(i -> findfirst(isequal(i),alph),collect(s))
+function rgb(r,g,b)
+    "\e[38;2;$(r);$(g);$(b)m"
+end
 
-rgb(r,g,b) =  "\e[38;2;$(r);$(g);$(b)m"
+function red()
+    rgb(255,0,0)
+end
 
-red() = rgb(255,0,0);
-yellow() = rgb(255,255,0);
-white() = rgb(255,255,255);
-gray(h) = rgb(h,h,h)
+function yellow()
+    rgb(255,255,0)
+end
 
-inv(f) = map(i -> findfirst(isequal(i),f),collect(1:length(f)))
+function white()
+    rgb(255,255,255)
+end
+
+function gray(h)
+    rgb(h,h,h)
+end
+
+
+
+function key(n)
+    copy(transpose(reduce(hcat, [Random.randperm(n) for i in 1:n])))
+end
+
+function inv(f)
+    map(i -> findfirst(isequal(i),f),collect(1:length(f)))
+end 
 
 function get_f(k)
     f = Int64[]
@@ -36,15 +59,14 @@ function get_f(k)
     f
 end
 
-
 function encode(p,q,F)
+    n = size(q)[begin]
     k = copy(q)
     c = Int64[]
     s = zeros(Int64,n)
     for i in eachindex(p)
         f = get_f(k)
         push!(c,f[p[i]] )
-        #c[i] = f[p[i]]
         f = circshift(f,p[i])
         push!(F,f)
         for j in 1:n k[j,:] = circshift(k[j,:], f[j]) end
@@ -53,6 +75,7 @@ function encode(p,q,F)
 end
 
 function decode(c,q)
+    n = size(q)[begin]
     k = copy(q)
     p = Int64[]
     for i in eachindex(c)
@@ -64,6 +87,7 @@ function decode(c,q)
     end
     p
 end
+
 function spin(k,r)
     n = size(k)[begin]
     s = zeros(Symbol,n)
@@ -89,8 +113,9 @@ function spin(q,r)
     k
 end
 
-function encrypt(p, q, r, F)
-    for i in 1:r
+function encrypt(p, q, F)
+    n = size(q)[begin]
+    for i in 1:n
         k = spin(q,i)
         p = encode(p,k,F)
         p = reverse(p)
@@ -98,31 +123,32 @@ function encrypt(p, q, r, F)
     p
 end
 
-function decrypt(p, q, r)
-    for i in 1:r
-        k = spin(q,r + 1 - i)
+function decrypt(p, q)
+    n = size(q)[begin]
+    for i in 1:n
+        k = spin(q,n + 1 - i)
         p = reverse(p)
         p = decode(p,k)
     end
     p
 end
+
 function demo()
+    n = 27
+    k = key(n)
 	print(white(),"k =\n",gray(100))
     printkey(k)
     for i in 1:20
         F = Set()
     	p = Random.randperm(n)
-    	c  = encrypt(p,k,r,F)
-    	d  = decrypt(c,k,r)
-    	print(white(),"f( ", red(), str_from_vec(p), white()," ) = ")
-    	print(yellow(),str_from_vec(c))
-        print(white(),@sprintf("  %d/%d\n", length(F), r*length(p)))
+    	c  = encrypt(p,k,F)
+    	d  = decrypt(c,k)
+    	print(white(),"f( ", red(), str_from_vec(p,""), white()," ) = ")
+    	print(yellow(),str_from_vec(c,""))
+        print(white(),@sprintf("  %d/%d\n", length(F), n*length(p)))
     	print(white())
     	if p != d @printf "ERROR" end
     end
 end
 
-alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
-n = length(alph)
-k = key(n)
-r = 5
+
